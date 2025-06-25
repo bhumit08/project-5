b2ef67a9149112a43575714a4243189c3bf04044
bhumit08 <bhumitthumar115@gmail.com>
2025-06-25 16:10:31 +0530

    filter product based on category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,76 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import {products} from './assets/frontend_assets/assets'
+
+// const App = () => {
+
+//   const showMen=(product)=>{
+//       product.filter((val)=>val.category==='Men')
+//       .map((ans)=>(
+//         <img src="ans.image"/>
+//       ))
+//   }
+
+
+//   return (
+//     <>
+//     Men <input type="checkbox" onClick={()=>showMen(products)}/><br/>
+//     Women <input type="checkbox"/><br/>
+//     Kids <input type="checkbox"/>
+
+
+
+//     <div className="flex flex-wrap">
+//     {
+//       products.map((val,key)=>(
+//         <ul key={val._id}>
+//           <img src={val.image} alt={'product'} className="m-10 size-30"/>
+//           <h4 className="ml-20">{val.brand}</h4>
+//         </ul>          
+//       ))
+//     }
+//     </div>
+
+//         </>
+
+//    );
+// };
+
+// export default App;
+
+import React, { useState } from "react";
+import { products } from './assets/frontend_assets/assets';
 
 const App = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
+  const handleCategoryChange = (category) => {
+    setSelectedCategories(prev =>
+      prev.includes(category)
+        ? prev.filter(cat => cat !== category)
+        : [...prev, category]
+    );
+  };
+
+  const filteredProducts = selectedCategories.length === 0
+    ? products
+    : products.filter(product => selectedCategories.includes(product.category));
 
   return (
     <>
-    <h1>hello</h1>
-    </>
+      Men <input type="checkbox" onChange={() => handleCategoryChange('Men')} /><br />
+      Women <input type="checkbox" onChange={() => handleCategoryChange('Women')} /><br />
+      Kids <input type="checkbox" onChange={() => handleCategoryChange('Kids')} /><br />
 
-   );
+      <div className="flex flex-wrap">
+        {filteredProducts.map((val) => (
+          <ul key={val._id}>
+            <img src={val.image} alt={'product'} className="m-10 size-30" />
+            <h4 className="ml-20">{val.brand}</h4>
+          </ul>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
