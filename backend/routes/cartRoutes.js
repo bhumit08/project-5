0aa4c77f0b83962934840ae82b3aa2b68e34f5a8
bhumit08 <bhumitthumar115@gmail.com>
2025-07-14 23:28:32 +0530

    complete the cart logic

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,4 +1,24 @@
-import express from 'express'
-import { addToCart,updateCart,getUserCart } from '../controllers/cartController'
+// import express from 'express'
+// import { addToCart,updateCart,getUserCart } from '../controllers/cartController'
+// import authUser from '../middleware/auth';
 
-const cartRouter=express.Router()
+// const cartRouter=express.Router();
+
+// cartRouter.post('/get',authUser,getUserCart)
+// cartRouter.post('/add',authUser,addToCart)
+// cartRouter.post('/update',authUser,updateCart)
+
+// export default cartRouter
+
+// cartRoutes.js
+import express from 'express';
+import { addToCart, updateCart, getUserCart } from '../controllers/cartController.js';
+import authUser from '../middleware/auth.js';
+
+const cartRouter = express.Router();
+
+cartRouter.post('/get', authUser, getUserCart);
+cartRouter.post('/add', authUser, addToCart);
+cartRouter.post('/update', authUser, updateCart);
+
+export default cartRouter;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +16,7 @@ connectCloudinary();
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use('/api/cart',cartRouter)
 
 // api endpoint
 app.use('/api/user',userRouter);
